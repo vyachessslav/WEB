77f657576a321db033811a4c77bebdc042a6674b
vyachessslav <93220240+vyachessslav@users.noreply.github.com>
2023-03-13 18:59:02 +0300

    Delete src directory

--- src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import {applyMiddleware, createStore} from "redux";
-import reducer from "./reducer";
-
-const thunkMiddleware = require('redux-thunk').default;
-
-export const store = createStore(reducer, applyMiddleware(thunkMiddleware));
--- src/service/isMobile.js
+++
@@ -1,7 +0,0 @@
-import {useMediaQuery} from "@mui/material";
-
-const isMobile = () => {
-    return useMediaQuery('(max-width: 800px)')
-}
-
-export default isMobile;
